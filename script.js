dc950a1f3f64be0c6369a0407709315bfc326371
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-15 18:47:17 -0300

    Corrigindo requisito 2 - implementando requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   function paletaDeCores() {
     let paleta = document.querySelector('#color-palette');
-    let cores = ['black', 'red', 'blue', 'green', 'yellow'];
+    let cores = ['black', 'red', 'green', 'yellow'];
 
     for (let posicao in cores) {
       let criaCor = document.createElement('li');
@@ -13,4 +13,26 @@ window.onload = function () {
   }
 
   paletaDeCores();
+
+  //! Function que implementa a paleta de cores e suas cores
+
+  function painel() {
+    let painel = document.querySelector('#pixel-board');
+    for (let index = 1; index <= 5; index += 1) {
+      let criaColuna = document.createElement('ol');
+      for (let indi = 1; indi <= 5; indi += 1) {
+        let criaLinha = document.createElement('li');
+        criaLinha.classList.add('pixel');
+        criaLinha.style.backgroundColor = 'white';
+
+        criaColuna.appendChild(criaLinha);
+      }
+
+      painel.appendChild(criaColuna);
+    }
+  }
+
+  painel();
+
+  //!  Function que implementa o painel para desenhar
 };
